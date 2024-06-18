20e3f6a28fc1e66b8bd2a0f99ffcf4b7acba2b1b
BeatriceV96 <beatricevanvitelli1@gmail.com>
2024-06-18 15:12:34 +0200

    Aggiungere i file di progetto.

---
+++ 18.06.24/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._06._24
+{
+    internal class Persona
+    {
+        string nome = "";
+        string cognome = "";
+        byte eta;
+
+        public string Nome { get { return nome; } set { nome = value; } }
+        public string Cognome { get { return cognome; } set { cognome = value; } }
+        public byte Eta { get { return eta; } set { eta = value; } }
+
+        public void Descrizione()
+        {
+            Console.WriteLine(" Nome: " + nome);
+        }
+        public void Descrizione(string cognome)
+        {
+            Console.WriteLine(" Cognome: " + cognome);
+        }
+        public void Descrizione(byte eta)
+        {
+            Console.WriteLine(" Anni: : " + eta);
+        }
+        public void Descrizione(string nome, string cognome, byte eta)
+        {
+            Console.WriteLine(" Descizione: " + nome + " " + cognome + " " + eta);
+        }
+    }
+}
+
---
+++ 18.06.24/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._06._24
+{
+    internal class Program
+    {
+
+            static void Main(string[] args)
+            {
+                Persona persona = new Persona();
+                persona.Nome = "Giovanni";
+                persona.Cognome = "Muciaccia";
+                persona.Eta = 40;
+
+                persona.Descrizione();
+                persona.Descrizione(persona.Cognome);
+                persona.Descrizione(persona.Eta);
+                persona.Descrizione(persona.Nome, persona.Cognome, persona.Eta);
+            }
+        }
+
+    }
